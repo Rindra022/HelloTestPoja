227fecbec9f8a41b4d52f148525634a6e03e3514
Rindra <hei.rindra.2@gmail.com>
2026-05-29 08:36:11 +0300

    feat: fix exception

--- src/main/java/com/my/company/service/ArithService.java
+++ src/main/java/com/my/company/service/ArithService.java
@@ -1,5 +1,6 @@
 package com.my.company.service;
 
+import com.my.company.exception.BadRequestException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,25 +8,35 @@ public class ArithService {
 
   public void checkPositiveNumber(Double a, Double b) {
     if (a < 0 || b < 0) {
-      throw new IllegalArgumentException("Number can't be negative");
+      throw new BadRequestException("Number can't be negative");
+    }
+  }
+
+  public void checkNotNull(Double a, Double b) {
+    if (a == null || b == null) {
+      throw new BadRequestException("Missing a parameter");
     }
   }
 
   public Double add(Double a, Double b) {
+    checkNotNull(a, b);
     checkPositiveNumber(a, b);
     return a + b;
   }
 
   public Double subtract(Double a, Double b) {
+    checkNotNull(a, b);
     checkPositiveNumber(a, b);
     return a - b;
   }
 
   public Double multiply(Double a, Double b) {
+    checkNotNull(a, b);
     return a * b;
   }
 
   public Double division(Double a, Double b) {
+    checkNotNull(a, b);
     checkPositiveNumber(a, b);
     return a / b;
   }
